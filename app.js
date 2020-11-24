802a0282eb806874fa5ee9bfdb87501f7683f5bd
Chris Chow <chrischow@Chriss-Mac-mini.local>
2020-11-24 06:31:46 -0800

    First Commit

---
+++ app.js
@@ -0,0 +1,131 @@
+//Set variables to select elements
+
+class Expense {
+  constructor(transaction, amount){
+    this.transaction = transaction;
+    this.amount = amount;
+  }
+}
+
+var calculateBtn = document.getElementById("btn-calculate");
+var expenseBtn = document.getElementById("btn-expense");
+var resetBtn = document.getElementById("reset");
+var budgetInput = document.getElementById("budget-test");
+var expense = document.getElementById("amount-input");
+var expenseArray = [];
+var expenseAmount = document.getElementById("expense-amount");
+var expenseType = document.getElementById("expense-input");
+
+
+// var budgetAmount = document.getElementById("budget-amount");
+
+//access budget value, parse to integer, post value to budget div
+function budgetAmount() {
+  var parsedBudgetAmount = parseInt(budgetInput.value).toLocaleString();
+  console.log(parsedBudgetAmount);
+  document.getElementById("budget-amount").innerHTML =
+    "$ " + parsedBudgetAmount;
+  console.log(budgetInput.value);
+}
+calculateBtn.addEventListener("click", budgetAmount);
+//add value from amount to expense container and push to empty array
+//  and update onclick event
+//add value from expense and amount and create new li tag, reset to zero
+function expenseFunction() {
+  expenseArray.push(expense.value); 
+  expenseAmount.innerHTML = expense.value;
+  console.log(expense.value);
+  var parsedArray = expenseArray.map(Number);
+  var reducedArray = parsedArray.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  document.getElementById("expense-amount").innerHTML =
+    "$ " + reducedArray.toLocaleString();
+
+  var outputBalance = budgetInput.value - reducedArray;
+
+  document.getElementById("balance-amount").innerHTML =
+    "$ " + outputBalance.toLocaleString();
+}
+
+expenseBtn.addEventListener("click", expenseFunction);
+
+function resetFunction() {
+  location.reload();
+  
+}
+
+resetBtn.addEventListener("click", resetFunction);
+
+//create body tag
+// function createTag() {
+//   let newElement = document.createElement("li");
+//   document.body.appendChild(newElement);
+//   expenseType.value = "";
+//   expenseAmount.value = "";
+//   expense.value = "";
+// }
+// expenseBtn.addEventListener("click", createTag);
+
+class UI {
+  // static displayExpense(){
+  //   const expense = Store.getExpense();
+
+  //   expense.forEach((expense) => UI.addExpenseToList(expense));
+  // }
+
+  static addExpenseToList(newExpense) {
+    const list = document.getElementById('items');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${newExpense.transaction}</td>
+    <td>${newExpense.amount}</td>
+    
+    `;
+
+    list.appendChild(row);
+  }
+  static deleteExpense(el) {
+    if(el.classList.contains('delete')){
+      el.parentElement.parentElement.remove();
+    }
+  }
+  static clearValues(){
+    //budgetInput.value = "";
+    expenseType.value = "";
+    expense.value = "";
+  }
+}
+
+//document.addEventListener('DOMContentLoaded', UI.displayExpense);
+
+expenseBtn.addEventListener("click", () => {
+  // Prevent actual submit
+  //e.preventDefault();
+  
+  // Get form values
+
+
+  // Validate
+  if(expenseType.value === '' || expense.value === '' ) {
+    alert('Fill in blanks');
+  } else {
+    // Instatiate book
+   const newExpense = new Expense(expenseType.value, expense.value);
+
+    // Add Book to UI
+    UI.addExpenseToList(newExpense);
+
+    // Add book to store
+    //Store.addBook(book);
+
+    // Show success message
+    //UI.showAlert('Book Added', 'success');
+
+    // Clear fields
+    UI.clearValues();
+  }
+});
+
+
